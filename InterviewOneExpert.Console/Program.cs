5f1ba04396bb8adfbf26f922fa8ab907ba830989
pszewczyk92 <pawel.p.szewczyk@wp.pl>
2026-01-27 21:49:46 +0100

    feat: demonstration of how it will work

--- InterviewOneExpert.Console/Program.cs
+++ InterviewOneExpert.Console/Program.cs
@@ -10,14 +10,14 @@ class Program
 
         var orderService = serviceProvider.GetRequiredService<IOrderService>();
 
-        // TODO: Demonstrate multi-threaded order processing
         Console.WriteLine("Order Processing System");
 
-        // Example: Simulate multiple threads processing orders
-        Task[] tasks = new Task[3];
-        tasks[0] = Task.Run(() => { /* Call ProcessOrder(1) */ });
-        tasks[1] = Task.Run(() => { /* Call ProcessOrder(2) */ });
-        tasks[2] = Task.Run(() => { /* Call ProcessOrder(-1) */ });
+        Task[] tasks =
+        [
+            Task.Run(() => { orderService.ProcessOrder(1); }),
+            Task.Run(() => { orderService.ProcessOrder(2); }),
+            Task.Run(() => { orderService.ProcessOrder(-1); }),
+        ];
         Task.WaitAll(tasks);
 
         Console.WriteLine("Processing complete.");
