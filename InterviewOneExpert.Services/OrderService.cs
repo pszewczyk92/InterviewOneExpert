c57e3a042563e504f21cea4fc9fa93ee56ce7241
pszewczyk92 <pawel.p.szewczyk@wp.pl>
2026-01-27 21:35:23 +0100

    feat: Order Service

---
+++ InterviewOneExpert.Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using InterviewOneExpert.Infrastructure.Abstraction;
+using InterviewOneExpert.Repositories.Abstraction;
+using InterviewOneExpert.Services.Abstraction;
+
+namespace InterviewOneExpert.Services;
+
+public class OrderService(IOrderRepository repository, ILogger logger) : IOrderService
+{
+    public void ProcessOrder(int orderId)
+    {
+        try
+        {
+            logger.LogInfo($"Starting processing for order ID {orderId}");
+
+            var order = repository.GetOrder(orderId);
+
+            logger.LogInfo($"Successfully processed order {orderId}: {order}");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Failed to process order ID {orderId}", ex);
+        }
+    }
+}
