9a55e781438d97ec07fbe694b58a54083a554db0
pszewczyk92 <pawel.p.szewczyk@wp.pl>
2026-01-27 21:26:58 +0100

    feat: logger implementation

---
+++ InterviewOneExpert.Infrastructure/Abstraction/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace InterviewOneExpert.Infrastructure.Abstraction;
+
+public interface ILogger
+{
+    void LogInfo(string message);
+    void LogError(string message, Exception ex);
+}
---
+++ InterviewOneExpert.Infrastructure/ConsoleLogger.cs
@@ -0,0 +1,18 @@
+﻿using InterviewOneExpert.Infrastructure.Abstraction;
+
+namespace InterviewOneExpert.Infrastructure;
+
+public class ConsoleLogger : ILogger
+{
+    public void LogInfo(string message)
+    {
+        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}] INFO  - {message}");
+    }
+
+    public void LogError(string message, Exception ex)
+    {
+        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}] ERROR - {message}");
+        Console.WriteLine($"           Exception: {ex.GetType().Name} | {ex.Message}");
+        Console.WriteLine($"           StackTrace: {ex.StackTrace}");
+    }
+}
